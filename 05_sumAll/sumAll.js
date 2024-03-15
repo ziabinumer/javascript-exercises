f2216b92bd7745697d5686c5c00503a0a0abf66e
Zia Gondal <atiqgondal@192.168.10.6>
2024-03-15 15:47:12 +0500

    Add Solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+
+    if (min > max) [min, max] = [max, min];
+
+    let sum = 0;
+    for (let i = min; i <= max; i++) sum += i;
+
+    return sum;
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
