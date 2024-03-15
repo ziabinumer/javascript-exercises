c64279c898abc84e2902b5b91951987dd73f27fa
Zia Gondal <atiqgondal@192.168.10.6>
2024-03-15 20:45:36 +0500

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '');
+    reverseWord = word.split("").reverse().join("");
+    if (word == reverseWord) return true;
+    return false;
 };
 
 // Do not edit below this line
