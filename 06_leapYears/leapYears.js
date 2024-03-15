5864a334ed5c643e989ca4adb51034b9c2a52ba6
Zia Gondal <atiqgondal@192.168.10.6>
2024-03-15 19:29:57 +0500

    Implement Solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeap = false;
+    if (year % 100 == 0 && year % 400 == 0) isLeap = true;
+    else if (year % 100 != 0 && year % 4 == 0) isLeap = true;
+
+    return isLeap;
 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
