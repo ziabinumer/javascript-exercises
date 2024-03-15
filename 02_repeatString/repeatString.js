cb237b68e44d8d4b8ee77c04e9b704f037dd6055
Zia Gondal <atiqgondal@192.168.10.7>
2024-03-15 13:25:23 +0500

    Update repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num >= 0)
+    {
+        let newString = "";
+        for (let i = 0; i < num; i++)
+        {
+            newString += string;
+        }
+        return newString;
+    }
+    return "ERROR";
 
 };
 
